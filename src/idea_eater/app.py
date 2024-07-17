65ca9cf2e2d50dfe3a17644fa0363cd78a1555d2
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-17 15:40:58 +0200

    .

--- src/idea_eater/app.py
+++ src/idea_eater/app.py
@@ -7,34 +7,40 @@ from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 import os
 import pathlib
+import locale
+from mylocale.TR import tr
+
+platform = toga.platform.current_platform
 
 
 class IdeaEater(toga.App):
-
     def startup(self):
+        self.file = f"{self.paths.app.absolute()}/resources/localisation.csv"
+        if platform == "android":
+            self.lang = str(
+                self._impl.native.getResources().getConfiguration().getLocales().get(0)
+            )  # for recognizing androids language settings
+        else:
+            self.lang = locale.getlocale()[0]
         main_box = toga.Box(style=Pack(direction=COLUMN))
-
-        idea_label = toga.Label("Thought goes here: ", style=Pack(padding=(0, 5)))
+        idea_label = toga.Label(
+            tr(csv_file=self.file, target_key="THOUGHTS", langcode=self.lang),
+            style=Pack(padding=(0, 5)),
+        )
         self.idea_input = toga.TextInput(style=Pack(flex=1))
-
         idea_box = toga.Box(style=Pack(direction=ROW, padding=5))
         idea_box.add(idea_label)
         idea_box.add(self.idea_input)
-
-        button = toga.Button("Nom", on_press=self.eat_idea, style=Pack(padding=5))
-
+        button = toga.Button(tr(csv_file=self.file, target_key="NOM", langcode=self.lang), on_press=self.eat_idea, style=Pack(padding=5))
         self.last_idea_label = toga.Label(
-            "Last idea eaten: NONE", style=Pack(padding=(0, 5))
+            tr(csv_file=self.file, target_key="LASTIDEAEATENNONE", langcode=self.lang), style=Pack(padding=(0, 5))
         )
-
         monster_image = toga.images.Image("resources/idea_eater.png")
         monster_image_view = toga.ImageView(id="monster_image", image=monster_image)
-
         main_box.add(idea_box)
         main_box.add(button)
         main_box.add(self.last_idea_label)
         main_box.add(monster_image_view)
-
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
@@ -42,9 +48,7 @@ class IdeaEater(toga.App):
     def eat_idea(self, widget):
         with open(os.path.join(pathlib.Path.home(), "ideas.txt"), "a") as f:
             f.write(self.idea_input.value + "\n")
-
-        self.last_idea_label.text = "Last idea eaten: " + self.idea_input.value
-
+        self.last_idea_label.text = tr(csv_file=self.file, target_key="LASTIDEAEATENNONE", langcode=self.lang) + self.idea_input.value
         self.idea_input.clear()
         self.idea_input.focus()
 
